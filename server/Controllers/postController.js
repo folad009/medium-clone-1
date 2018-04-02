6cbe0d2416caa75106e43cb5266ff638bf339e03
RyanD860 <31831221+RyanD860@users.noreply.github.com>
2018-04-02 14:46:14 -0400

    Working on session and auth

---
+++ server/Controllers/postController.js
@@ -0,0 +1,28 @@
+module.exports = {
+  // WILL RETRIEVE ALL POSTS
+  getPosts: function(req, res, next) {
+    const db = req.app.get("db");
+
+    db
+      .getPosts()
+      .then(response => {
+        res.status(200).json(response);
+      })
+      .catch(res.status(400));
+  },
+
+  // WILL ADD POST TO POST TABLE
+
+  addPost: function(req, res, next) {
+    const db = req.app.get("db");
+    if (req.body.categories === "") {
+      req.body.categories = null;
+    }
+    db
+      .addPost(req.body.title, req.body.body, req.body.categories)
+      .then(response => {
+        res.status(200).send(response);
+      })
+      .catch(res.status(400));
+  }
+};
--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const massive = require("massive");
 const session = require("express-session");
 const cors = require("cors");
 const passport = require("passport");
+const strategy = require(`${__dirname}/strategy`);
+
+// BRINGING IN CONTROLLERS
+const postController = require(`${__dirname}/Controllers/postController`);
+const userController = require(`${__dirname}/Controllers/userController`);
 
 // PORT
 
@@ -20,9 +25,9 @@ app.use(cors());
 // CONNECTING TO HEROKU DATABSE
 massive(process.env.CONNECTION_STRING)
   .then(db => {
-    app.set("dbInstance", db);
+    app.set("db", db);
   })
-  .catch(console.log({ message: "Connection to database failed" }));
+  .catch(console.log);
 
 // CREATING A SESSION
 
@@ -35,8 +40,100 @@ app.use(
   })
 );
 
+// INITIALIZING PASSPORT
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(strategy);
+
+passport.serializeUser((profile, done) => {
+  done(null, profile);
+});
+
+passport.deserializeUser((profile, done) => {
+  done(null, profile);
+});
+// END POINTS
+
+// GET
+
+// GETS ALL POSTS
+app.get("/api/getposts", postController.getPosts);
+app.get("/api/getuser", (req, res, next) => {
+  if (req.session.user) {
+    app
+      .get("db")
+      .getUser([req.session.user.userid])
+      .then(response => {
+        res.status(200).send(response);
+      });
+  } else {
+    res.status(400).send("No user found");
+  }
+});
+
+// POST
+
+// ADDS POST
+app.post("/api/addpost", postController.addPost);
+
+// PUT
+
+// DELETE
+
+// AUTHENTICATION
+app.get(
+  "/login",
+  passport.authenticate("auth0", {
+    successRedirect: "/me",
+    failureRedirect: "/login",
+    failureFlash: true
+  })
+);
+
+app.get("/me", function(req, res, next) {
+  if (!req.user.id) {
+    res.redirect("/login");
+  } else {
+    app
+      .get("db")
+      .getUser([req.user.id])
+      .then(response => {
+        if (!response[0]) {
+          app
+            .get("db")
+            .addUser([
+              req.user.id,
+              req.user.name.givenName,
+              req.user.name.familyName
+            ])
+            .then(response => {
+              console.log(response);
+              req.session.user = { userid: response[0].id };
+              console.log(req.session);
+            })
+            .then(res.redirect("http://localhost:3000"));
+          next();
+        } else {
+          req.session.user = {
+            userid: response[0].id
+          };
+          console.log(req.session);
+          res.redirect("http://localhost:3000");
+        }
+      });
+  }
+});
+
+app.get("/api/logout", (req, res, next) => {
+  req.session.destroy();
+  next();
+  res.status(200);
+});
+
 // SERVER LISTENING
 
-app.listen(port => {
+app.listen(port, () => {
   console.log(`Currently listening on port #${port}`);
 });
