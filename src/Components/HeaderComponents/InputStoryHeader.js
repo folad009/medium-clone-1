fb9cbc1b76d814f7cfebf5bece7fa584b9335931
Cococody <mayo.cody@gmail.com>
2018-04-02 16:35:44 -0500

    Header Links

--- src/Components/HeaderComponents/InputStoryHeader.js
+++ src/Components/HeaderComponents/InputStoryHeader.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import IoIconPack from 'react-icons/lib/io';
-import Dots from 'react-icons/lib/io/ios-more'
-import Bookmark from 'react-icons/lib/io/android-bookmark';
-import Notification from 'react-icons/lib/io/android-notifications-none';
-import Logo from '../../assets/logo.svg'
+import React from "react";
+import IoIconPack from "react-icons/lib/io";
+import Dots from "react-icons/lib/io/ios-more";
+import Bookmark from "react-icons/lib/io/android-bookmark";
+import Notification from "react-icons/lib/io/android-notifications-none";
+import Logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
 
-function InputStoryHeader(){
-    return(
-        <div className="input-story-header-component-main-div">
-            <div className="input-story-header-logo-left-div">
-                <img src={Logo} className="logo-small"/>
-                <p>Save</p>
-            </div>
-            <div className="story-header-right-side-div">
-            <p>Share</p>
-            <p>Publish</p>
-            <Dots className="story-header-icons"/>
-            <Bookmark className="story-header-icons"/>
-            <Notification className="story-header-icons"/>
-            <img className="user-image"/>
-            </div>
-        </div>
-    )
+function InputStoryHeader() {
+  return (
+    <div className="input-story-header-component-main-div">
+      <div className="input-story-header-logo-left-div">
+        <Link to="/">
+          <img src={Logo} className="logo-small" />
+        </Link>
+        <p>Save</p>
+      </div>
+      <div className="story-header-right-side-div">
+        <p>Share</p>
+        <p>Publish</p>
+        <Dots className="story-header-icons" />
+        <Bookmark className="story-header-icons" />
+        <Notification className="story-header-icons" />
+        <Link to="/@user">
+          <img className="user-image" />
+        </Link>
+      </div>
+    </div>
+  );
 }
+
 export default InputStoryHeader;
