b937ab09aa47e314e5d7aa373d3135e81f0febfa
Jpecina <31221531+Jpecina@users.noreply.github.com>
2018-04-02 13:35:00 -0500

    added view

--- src/Components/StoryRenderComponent/StoryRenderComponent.js
+++ src/Components/StoryRenderComponent/StoryRenderComponent.js
@@ -6,9 +6,19 @@ import FacebookIcon from 'react-icons/lib/io/social-facebook-outline'
 
 function StoryRenderComponent(){
     return(
-        <div className="story-render-component-main-div">
+        <div >
            <div className="story-render-component-body">
-           {/* Content goes here*/}
+
+           <ul>
+           <li>wcwcwc<br/>
+           </li>
+           <li>cwwwc</li>
+           <li>wcwqcwcw</li>
+           </ul>
+           <img src="https://wallpaperbrowse.com/media/images/cat-1285634_960_720.png" style={{fontSize:"1.5em"}}/>
+           <br/>
+           <h1>
+           <br/>sjdksandkasndsa</h1><div><u><b>dqwdwqdwqdwd</b></u></div><div><br/></div>
            </div>
             <div className="story-render-component-clap-section">
             <div className="story-render-component-clap-section-text">
