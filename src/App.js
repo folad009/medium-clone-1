3bc028320c18eac18a5fcaceb2c69c4ad3982f95
Jpecina <31221531+Jpecina@users.noreply.github.com>
2018-04-02 13:05:37 -0500

    added story view

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AddStoryView from './Views/AddStoryView';
+// import AddStoryView from './Views/AddStoryView';
+import StoryReadView from './Views/StoryReadView';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AddStoryView/>        
+        <StoryReadView/>        
       </div>
     );
   }
---
+++ src/Components/StoryRenderComponent/StoryRenderComponent.js
@@ -0,0 +1,29 @@
+import React from  'react';
+import MainHeader from '../HeaderComponents/MainHeader';
+import ChatIcon from 'react-icons/lib/io/ios-chatbubble-outline';
+import TwitterIcon from "react-icons/lib/io/social-twitter-outline";
+import FacebookIcon from 'react-icons/lib/io/social-facebook-outline'
+
+function StoryRenderComponent(){
+    return(
+        <div className="story-render-component-main-div">
+           <div className="story-render-component-body">
+           {/* Content goes here*/}
+           </div>
+            <div className="story-render-component-clap-section">
+            <div className="story-render-component-clap-section-text">
+                <h4>One clap, two clap, three clap, forty?</h4>
+                <p>By clapping more or less, you can signal to us which stories really stand out.</p>
+            </div>
+            <div className="story-render-component-clap-section-icons-div">
+                <img/>
+                <ChatIcon className="story-header-icons"/>
+                <p>3</p>
+                <TwitterIcon className="story-header-icons"/>
+                <FacebookIcon className="story-header-icons"/>
+            </div>
+            </div>
+        </div>
+    )
+}
+export default StoryRenderComponent;
