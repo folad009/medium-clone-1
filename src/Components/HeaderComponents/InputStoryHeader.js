e114ff606566f681f954f4275a43bb450cfd8f86
Jpecina <31221531+Jpecina@users.noreply.github.com>
2018-04-01 21:18:52 -0500

    added logo small to input page

--- src/Components/HeaderComponents/InputStoryHeader.js
+++ src/Components/HeaderComponents/InputStoryHeader.js
@@ -2,20 +2,22 @@ import React from 'react';
 import IoIconPack from 'react-icons/lib/io';
 import Dots from 'react-icons/lib/io/ios-more'
 import Bookmark from 'react-icons/lib/io/android-bookmark';
+import Notification from 'react-icons/lib/io/android-notifications-none';
+import Logo from '../../assets/logo.svg'
 
 function InputStoryHeader(){
     return(
         <div className="input-story-header-component-main-div">
             <div className="input-story-header-logo-left-div">
-                <img alt="icon goes here"/>
-                <p>save</p>
+                <img src={Logo} className="logo-small"/>
+                <p>Save</p>
             </div>
             <div className="story-header-right-side-div">
             <p>Share</p>
             <p>Publish</p>
             <Dots className="story-header-icons"/>
             <Bookmark className="story-header-icons"/>
-            <img className="user-image"/>
+            <Notification className="story-header-icons"/>
             <img className="user-image"/>
             </div>
         </div>
