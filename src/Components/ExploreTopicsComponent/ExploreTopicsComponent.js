7f14a42d8dcb8e7436db877534b11800c9d30374
Cococody <mayo.cody@gmail.com>
2018-04-03 10:19:30 -0500

    Began explore topics view

---
+++ src/Components/ExploreTopicsComponent/ExploreTopicsComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class ExploreTopicsComponent extends React.Component {
+  render() {
+    return <div>Explore Topics</div>;
+  }
+}
+
+export default ExploreTopicsComponent;
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from "react-router-dom";
 import AddStoryView from "./Views/AddStoryView";
 import HomePageView from "./Views/HomePageView";
 import StoryReadView from "./Views/StoryReadView";
+import ExploreTopicsView from "./Views/ExploreTopicsView";
 
 export default (
   <Switch>
     <Route exact path="/" exact component={HomePageView} />
     <Route path="/new-story" component={AddStoryView} />
-    <Route path="/story-view" component={StoryReadView}/>
+    <Route path="/story-view" component={StoryReadView} />
+    <Route path="/topics" component={ExploreTopicsView} />
   </Switch>
 );
