83600aeb2f59bac97f243145c199a356128f97b7
Jonathan <jaquin92@yahoo.com>
2018-04-04 17:00:42 -0500

    change

--- db/addPost.sql
+++ db/addPost.sql
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-INSERT INTO posts (userid,title, body, categories) VALUES ($1, $2, $3, $4) RETURNING *;
-=======
 INSERT INTO posts (userid,title, body, categories) VALUES ($1, $2, $3,$4) RETURNING *;
->>>>>>> master
