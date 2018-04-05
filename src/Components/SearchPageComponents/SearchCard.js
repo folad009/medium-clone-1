611c94c0d8b1b4a769b57254c6def08dd0baf856
Cococody <mayo.cody@gmail.com>
2018-04-05 14:37:09 -0500

    Started on search page

---
+++ src/Components/SearchPageComponents/SearchCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class SearchCard extends React.Component {
+  render() {
+    return (
+      <div className="search-card">
+        <div className="search-card-title">Whoever</div>
+        <div className="search-card-content">
+          <div className="search-card-inner-content">
+            <div className="search-card-image" />
+            <h1>Stuff</h1>
+            <h3>Description of things to come</h3>
+          </div>
+        </div>
+        <h5>Read More</h5>
+        <div className="search-card-claps-and-responses">clap 35K</div>
+        card
+      </div>
+    );
+  }
+}
+
+export default SearchCard;
--- src/Components/SearchPageComponents/SearchPage.js
+++ src/Components/SearchPageComponents/SearchPage.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { withRouter, Link } from "react-router-dom";
+import "./SearchPage.css";
+import SearchCard from "./SearchCard";
 
 class SearchPage extends React.Component {
   render() {
     return (
       <div className="search-page-main-div">
         <h1>Search</h1>
-        {console.log(this.props)}
+        <SearchCard />
+        <SearchCard />
+        {console.log(this.props.location)}
       </div>
     );
   }
