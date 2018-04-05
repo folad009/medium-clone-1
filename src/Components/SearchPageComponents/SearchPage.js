2db8f1c1f7e480b63c9e650b3dac83774816be18
Cococody <mayo.cody@gmail.com>
2018-04-05 16:28:00 -0500

    search page styled and ready to function

--- src/Components/SearchPageComponents/SearchPage.js
+++ src/Components/SearchPageComponents/SearchPage.js
@@ -2,18 +2,51 @@ import React from "react";
 import { withRouter, Link } from "react-router-dom";
 import "./SearchPage.css";
 import SearchCard from "./SearchCard";
+import { connect } from "react-redux";
+import { getAllPosts } from "../../ducks/reducer";
 
 class SearchPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      userInput: ""
+    };
+  }
+  componentDidMount() {
+    this.props
+      .getAllPosts()
+      .then(response => this.setState({ posts: this.props.posts }));
+  }
   render() {
+    let searchReel =
+      this.state.posts.length > 0
+        ? this.state.posts.map((val, index) => (
+            <SearchCard
+              authorName={`${val.firstname} ${val.lastname}`}
+              body={val.body}
+              title={val.title}
+              img={val.thumbnailimg}
+              date={val.date}
+              key={index}
+              id={val.id}
+            />
+          ))
+        : "";
     return (
       <div className="search-page-main-div">
         <h1>Search</h1>
-        <SearchCard />
-        <SearchCard />
+
+        {searchReel}
         {console.log(this.props.location)}
       </div>
     );
   }
 }
+function mapStateToProps(state) {
+  return state;
+}
 
-export default withRouter(SearchPage);
+export default withRouter(
+  connect(mapStateToProps, { getAllPosts })(SearchPage)
+);
