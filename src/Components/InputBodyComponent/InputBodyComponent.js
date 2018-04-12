073a656ebc2d44c83793f8fd507adabab68b5826
Cococody <mayo.cody@gmail.com>
2018-04-12 15:08:45 -0500

    write-story complete

--- src/Components/InputBodyComponent/InputBodyComponent.js
+++ src/Components/InputBodyComponent/InputBodyComponent.js
@@ -6,13 +6,13 @@ import Bookmark from "react-icons/lib/io/android-bookmark";
 import Notification from "react-icons/lib/io/android-notifications-none";
 import Logo from "../../assets/logo.svg";
 import InputStoryHeader from "../HeaderComponents/InputStoryHeader";
+
 import { getUser } from "../../ducks/reducer";
 import { connect } from "react-redux";
-import firebase from 'firebase';
-import FileUploader from 'react-firebase-file-uploader';
-require('medium-editor/dist/css/medium-editor.css');
-require('medium-editor/dist/css/themes/default.css');
-
+import firebase from "firebase";
+import FileUploader from "react-firebase-file-uploader";
+require("medium-editor/dist/css/medium-editor.css");
+require("medium-editor/dist/css/themes/default.css");
 
 class InputBodyComponent extends Component {
   constructor() {
@@ -21,39 +21,47 @@ class InputBodyComponent extends Component {
       title: "",
       body: "",
       categories: [],
-      username: '',
-      avatar: '',
+      username: "",
+      avatar: "",
       isUploading: false,
       progress: 0,
-      avatarURL: ''
+      avatarURL: ""
     };
   }
-  componentDidMount() {
-
-  }
+  componentDidMount() {}
 
   addCategory(str) {
-    console.log("cats", this.state.categories)
+    console.log("cats", this.state.categories);
     let cats = this.state.categories;
-    cats.push(str)
-    this.setState({ categories: cats })
+    cats.push(str);
+    this.setState({ categories: cats });
   }
-
-  handleChangeUsername = (event) => this.setState({ username: event.target.value });
+  removeCategory(str) {
+    let cats = this.state.categories;
+    let deleteIndex = cats.indexOf(val => val == str);
+    cats.splice(deleteIndex, 1);
+    this.setState({ categories: cats });
+  }
+  handleChangeUsername = event =>
+    this.setState({ username: event.target.value });
   handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
-  handleProgress = (progress) => this.setState({ progress });
-  handleUploadError = (error) => {
+  handleProgress = progress => this.setState({ progress });
+  handleUploadError = error => {
     this.setState({ isUploading: false });
     console.error(error);
-  }
-  handleUploadSuccess = (filename) => {
+  };
+  handleUploadSuccess = filename => {
     this.setState({ avatar: filename, progress: 100, isUploading: false });
-    firebase.storage().ref('images').child(filename).getDownloadURL().then(url => {
-
-      this.setState({
-        avatarURL: url
-      })
-    });
+    firebase
+      .storage()
+      .ref("images")
+      .child(filename)
+      .getDownloadURL()
+      .then(url => {
+        this.setState({
+          avatarURL: url
+        });
+      });
   };
 
   handleChanges = (text, medium) => {
@@ -66,52 +74,70 @@ class InputBodyComponent extends Component {
 
   render() {
     let categoryReel = this.props.categories.map((item, i) => {
-      if(this.state.categories.includes(item.name)){
-        return <div className="cat-button-selected">{item.name.toUpperCase()}</div>
+      if (this.state.categories.includes(item.name)) {
+        return (
+          <div
+            className="cat-button-selected"
+            onClick={() => {
+              this.removeCategory(item.name);
+            }}
+          >
+            {item.name.toUpperCase()}
+          </div>
+        );
       }
       return (
-        <div className="cat-selector-button-div" onClick={() => this.addCategory(item.name)} >
-       {item.name.toUpperCase()}
+        <div
+          className="cat-selector-button-div"
+          onClick={() => this.addCategory(item.name)}
+        >
+          {item.name.toUpperCase()}
         </div>
-      )
-    })
+      );
+    });
     console.log(this.state);
     return (
       <div>
-
         {this.state.avatarURL}
-
-        <InputStoryHeader img={this.state.avatarURL} title={this.state.title} body={this.state.body} cats={this.state.categories} />
+        <InputStoryHeader
+          img={this.state.avatarURL}
+          title={this.state.title}
+          body={this.state.body}
+          cats={this.state.categories}
+        />
         <div className="input-body-component-main-div">
+          <div className="category-selector-body">
+            <div className="categry-selector-sub-body">
+              <div className="category-selector-header">
+                <h3>Select Categories</h3>
+              </div>
+              <div className="category-selector-main-div">{categoryReel}</div>
+            </div>
+          </div>
           <div className="input-body-component-user-info">
             {this.props.user.id ? (
               <img className="user-image" src={this.props.user.avatar} />
             ) : (
-                false
-              )}
+              false
+            )}
             <div className="input-body-component-user-info-name">
               <p>Draft</p>
             </div>
             <form>
-
-              {this.state.isUploading &&
-                <p>Progress: {this.state.progress}</p>
-              }
+              {this.state.isUploading && <p>Progress: {this.state.progress}</p>}
 
               <FileUploader
                 accept="image/*"
                 name="thumbnail"
                 randomizeFilename
-                storageRef={firebase.storage().ref('images')}
+                storageRef={firebase.storage().ref("images")}
                 onUploadStart={this.handleUploadStart}
                 onUploadError={this.handleUploadError}
                 onUploadSuccess={this.handleUploadSuccess}
                 onProgress={this.handleProgress}
               />
             </form>
-
           </div>
-
 
           <Editor
             className="input-body-component-title"
@@ -141,11 +167,6 @@ class InputBodyComponent extends Component {
               }
             }}
           />
-        </div>
-        <div className="category-selector-body">
-          <div className="category-selector-main-div">
-            {categoryReel}
-          </div>
         </div>
       </div>
     );
