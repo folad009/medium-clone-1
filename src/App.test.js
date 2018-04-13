9bc26011d0833242af9236053a7c8a397ad25a6b
Jonathan <jaquin92@yahoo.com>
2018-04-13 14:27:37 -0500

    jonathan

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
+import { HashRouter } from "react-router-dom";
+import * as firebase from 'firebase';
+firebase.initializeApp(config);
 import { shallow } from "enzyme";
 import { TopicHeaderBar } from "./Components/HeaderComponents/TopicHeaderBar";
+var config = {
+  apiKey: "AIzaSyD5BMrvAXkMRA1NpaqPiTHtdhV4qo4gDP4",
+  authDomain: "medium-c.firebaseapp.com",
+  databaseURL: "https://medium-c.firebaseio.com",
+  projectId: "medium-c",
+  storageBucket: "medium-c.appspot.com",
+  messagingSenderId: "682649004694"
+};
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
