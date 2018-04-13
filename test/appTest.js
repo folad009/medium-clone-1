ae02cee52f1032c5d1826aa99e845b6ce5e6c256
Cococody <mayo.cody@gmail.com>
2018-04-13 14:10:54 -0500

    merging

--- test/appTest.js
+++ test/appTest.js
@@ -1,5 +1,6 @@
 const assert = require("chai").assert;
 const app = require("./app");
+const reducer = require("../src/ducks/reducer");
 
 describe("App", function() {
   it("app should return hello", function() {
