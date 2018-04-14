1eb98ce13d25b8a08add7004da6407c7e7319799
RyanD860 <31831221+RyanD860@users.noreply.github.com>
2018-04-13 20:38:17 -0400

    Commit before merge

--- test/functionTest.js
+++ test/functionTest.js
@@ -1,9 +1,14 @@
 var expect = require("chai").expect;
 var assert = require("chai").assert;
+var should = require("chai").should;
+let promise;
+var getCategories = require("./../src/ducks/reducer").getCategories;
 
-describe("gets all categories", function() {
+describe("Get Category function of reducer", function() {
   it("It returns true value", function() {
-    var getCategories = require("./../src/ducks/reducer").getCategories;
     expect(getCategories()).to.be.ok;
+  });
+  it("Calls the correct action type to the reducer", function() {
+    expect(getCategories().type).to.eql("GET_CATEGORIES");
   });
 });
