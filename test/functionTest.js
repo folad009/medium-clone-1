be6308244595f3da5d70dbb8f58ae4c3f10fd104
Jonathan <jaquin92@yahoo.com>
2018-04-16 11:54:15 -0500

    unit testing done, Lesketit

--- test/functionTest.js
+++ test/functionTest.js
@@ -9,6 +9,10 @@ var getUserInterests = require("./../src/ducks/reducer").getUserInterests;
 var getAllPosts = require("./../src/ducks/reducer").getAllPosts;
 var getUserFollowing = require("./../src/ducks/reducer").getUserFollowing;
 
+
+
+
+
 const initialState = {
   user: {},
   userFollowing: [],
@@ -18,57 +22,57 @@ const initialState = {
   readingList: []
 };
 
-describe("Get Category function of reducer", function() {
-  it("It returns true value", function() {
+describe("Get Category function of reducer", function () {
+  it("It returns true value", function () {
     expect(getCategories()).to.be.ok;
   });
-  it("Calls the correct action type to the reducer", function() {
+  it("Calls the correct action type to the reducer", function () {
     expect(getCategories().type).to.eql("GET_CATEGORIES");
   });
 });
 
-describe("User interest method on the reducer", function() {
-  it("It returns true value", function() {
+describe("User interest method on the reducer", function () {
+  it("It returns true value", function () {
     expect(getUserInterests()).to.be.ok;
   });
-  it("Calls the correct action type to the reducer", function() {
+  it("Calls the correct action type to the reducer", function () {
     expect(getUserInterests().type).to.eql("GET_USER_INTERESTS");
   });
 });
 
-describe("Gets all posts", function() {
-  it("Calls the correct action type to the reducer", function() {
+describe("Gets all posts", function () {
+  it("Calls the correct action type to the reducer", function () {
     expect(getAllPosts().type).to.eql("GET_ALL_POSTS");
   });
 });
 
-describe("gets all posts", function() {
-  it("It returns true value", function() {
+describe("gets all posts", function () {
+  it("It returns true value", function () {
     var getAllPosts = require("./../src/ducks/reducer").getAllPosts;
     expect(getAllPosts()).to.be.ok;
   });
 });
 
-describe("retrieves user", function() {
-  it("retreives user", function() {
+describe("retrieves user", function () {
+  it("retreives user", function () {
     var getUser = require("./../src/ducks/reducer").getUser;
     expect(getUser()).to.be.ok;
   });
 
-  it("calls the correct action type on the reducer", function() {
+  it("calls the correct action type on the reducer", function () {
     var getUser = require("./../src/ducks/reducer").getUser;
     expect(getUser().type).to.eql("GET_USER");
   });
 });
 
-describe("get user following", function() {
-  it("User following returns a true value", function() {
+describe("get user following", function () {
+  it("User following returns a true value", function () {
     expect(getUserFollowing()).to.be.ok;
   });
-  it("Calls the correct action type on the reducer", function() {
+  it("Calls the correct action type on the reducer", function () {
     expect(getUserFollowing().type).to.eql("GET_USER_FOLLOWING");
   });
-  it("expects reducer call to return promise", function() {
+  it("expects reducer call to return promise", function () {
     axios
       .get("/api/userfollowing/37")
       .then(response => {
@@ -78,25 +82,52 @@ describe("get user following", function() {
   });
 });
 
-describe("test Reading List ", function() {
-  it("adds to reading list ", function() {
+describe("test Reading List ", function () {
+  it("adds to reading list ", function () {
     var addToReadingList = require("./../src/ducks/reducer").addToReadingList;
     expect(addToReadingList()).to.be.ok;
   });
 
-  it("Calls the correct action type to the reducer", function() {
+  it("Calls the correct action type to the reducer", function () {
     var addToReadingList = require("./../src/ducks/reducer").addToReadingList;
     expect(addToReadingList().type).to.eql("ADD_TO_READING_LIST");
   });
-  it("deletes from reading list", function() {
+  it("deletes from reading list", function () {
     var deleteFromReadingList = require("./../src/ducks/reducer")
       .deleteFromReadingList;
     expect(deleteFromReadingList()).to.be.ok;
   });
 
-  it("Calls the correct action type to the reducer", function() {
+  it("Calls the correct action type to the reducer", function () {
     var deleteFromReadingList = require("./../src/ducks/reducer")
       .deleteFromReadingList;
     expect(deleteFromReadingList().type).to.eql("DELETE_FROM_READING_LIST");
+  });
+});
+
+
+
+
+describe("retrieves reading list", function () {
+  it("retrieves reading list", function () {
+
+    expect(reducer.getReadingList()).to.be.ok;
+  });
+
+  it("calls the correct action type on the reducer", function () {
+
+    expect(reducer.getReadingList().type).to.eql("GET_READING_LIST");
+  });
+});
+
+describe("log out", function () {
+  it("log out", function () {
+
+    expect(reducer.logOut()).to.be.ok;
+  });
+
+  it("calls the correct action type on the reducer", function () {
+
+    expect(reducer.logOut().type).to.eql("LOGOUT");
   });
 });
