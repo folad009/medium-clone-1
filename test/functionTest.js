1f7c874bb52a67c40f6aa3a91853cc0944286bc1
RyanD860 <31831221+RyanD860@users.noreply.github.com>
2018-04-16 10:37:02 -0400

    Adding Unit tests

--- test/functionTest.js
+++ test/functionTest.js
@@ -3,6 +3,8 @@ var assert = require("chai").assert;
 var should = require("chai").should;
 let promise;
 var getCategories = require("./../src/ducks/reducer").getCategories;
+var getUserInterests = require("./../src/ducks/reducer").getUserInterests;
+var getAllPosts = require("./../src/ducks/reducer").getAllPosts;
 
 describe("Get Category function of reducer", function() {
   it("It returns true value", function() {
@@ -13,6 +15,21 @@ describe("Get Category function of reducer", function() {
   });
 });
 
+describe("User interest method on the reducer", function() {
+  it("It returns true value", function() {
+    expect(getUserInterests()).to.be.ok;
+  });
+  it("Calls the correct action type to the reducer", function() {
+    expect(getUserInterests().type).to.eql("GET_USER_INTERESTS");
+  });
+});
+
+describe("Gets all posts", function() {
+  it("Calls the correct action type to the reducer", function() {
+    expect(getAllPosts().type).to.eql("GET_ALL_POSTS");
+  });
+});
+
 describe("gets all posts", function() {
   it("It returns true value", function() {
     var getAllPosts = require("./../src/ducks/reducer").getAllPosts;
@@ -20,12 +37,15 @@ describe("gets all posts", function() {
   });
 });
 
-
-
 describe("retrieves user", function() {
   it("retreives user", function() {
     var getUser = require("./../src/ducks/reducer").getUser;
     expect(getUser()).to.be.ok;
+  });
+
+  it("calls the correct action type on the reducer", function() {
+    var getUser = require("./../src/ducks/reducer").getUser;
+    expect(getUser().type).to.eql("GET_USER");
   });
 });
 
@@ -34,10 +54,9 @@ describe("test Reading List ", function() {
     var addToReadingList = require("./../src/ducks/reducer").addToReadingList;
     expect(addToReadingList()).to.be.ok;
   });
-  it("deletes from reading list",function(){
-    var deleteFromReadingList = require("./../src/ducks/reducer").deleteFromReadingList;
+  it("deletes from reading list", function() {
+    var deleteFromReadingList = require("./../src/ducks/reducer")
+      .deleteFromReadingList;
     expect(deleteFromReadingList()).to.be.ok;
   });
-  
 });
-
